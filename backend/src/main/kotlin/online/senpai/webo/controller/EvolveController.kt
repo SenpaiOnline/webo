152fe48787abffa4ec46c9fa4217f198e46ece35
Maxim Pavlov <maxpav137@gmail.com>
2021-11-10 17:11:00 +0500

    #WEBO-12 feat(backend): add EvolveController

---
+++ backend/src/main/kotlin/online/senpai/webo/controller/EvolveController.kt
@@ -0,0 +1,35 @@
+package online.senpai.webo.controller
+
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.PathVariable
+import io.micronaut.http.annotation.QueryValue
+import io.micronaut.security.annotation.Secured
+import io.micronaut.security.rules.SecurityRule
+import io.micronaut.validation.Validated
+import jakarta.inject.Inject
+import online.senpai.webo.EvolveCharacter
+import online.senpai.webo.dto.evolve.EvolveDialogueDto
+import online.senpai.webo.service.EvolveService
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+
+@Validated
+@Secured(SecurityRule.IS_ANONYMOUS)
+@Controller("/api/evolve")
+class EvolveController @Inject constructor(
+    private val service: EvolveService,
+) {
+    @Get("/meta/dialogues/total/{character}")
+    fun countDialoguesByCharacter(
+        @PathVariable character: EvolveCharacter
+    ): Mono<Number> =
+        service.countByCharacter(character)
+
+    @Get("/dialogues/{character}")
+    fun findTwentyDialoguesByCharacter(
+        @PathVariable character: EvolveCharacter,
+        @QueryValue(defaultValue = "0") offset: Long
+    ): Flux<EvolveDialogueDto> =
+        service.findTwentyDialoguesByCharacter(character, offset)
+}
