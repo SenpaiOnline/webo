adaa00cddf494ca5982a50b690e04c424579ae4b
Maxim Pavlov <maxpav137@gmail.com>
2021-12-04 17:01:49 +0500

    #WEBO-16 refactor(backend): change the behavior of the EvolveController. Now it returns an EvolveDialoguesMetaDto instead of a plain number

---
+++ backend/src/main/kotlin/online/senpai/webo/dto/evolve/EvolveDialoguesMetaDto.kt
@@ -0,0 +1,6 @@
+package online.senpai.webo.dto.evolve
+
+data class EvolveDialoguesMetaDto(
+    val rowsNumber: Long,
+    val rowsPerPage: Long
+)
--- backend/src/main/kotlin/online/senpai/webo/service/EvolveService.kt
+++ backend/src/main/kotlin/online/senpai/webo/service/EvolveService.kt
@@ -1,11 +1,12 @@
 package online.senpai.webo.service
 
-import online.senpai.webo.EvolveCharacter
 import online.senpai.webo.dto.evolve.EvolveDialogueDto
+import online.senpai.webo.dto.evolve.EvolveDialoguesMetaDto
+import online.senpai.webo.misc.EvolveCharacter
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 interface EvolveService {
-    fun countByCharacter(character: EvolveCharacter): Mono<Number>
+    fun characterMeta(character: EvolveCharacter): Mono<EvolveDialoguesMetaDto>
     fun findTwentyDialoguesByCharacter(character: EvolveCharacter, offset: Long): Flux<EvolveDialogueDto>
 }
