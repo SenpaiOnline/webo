b2b7621acbe858ae779c8660d324192e264687c0
Maxim Pavlov <maxpav137@gmail.com>
2022-03-13 01:30:00 +0500

    WEBO-21 refactor(backend): add missed Introspected annotation to EvolveDialoguesMetaDto class

--- backend/src/main/kotlin/online/senpai/webo/dto/evolve/EvolveDialoguesMetaDto.kt
+++ backend/src/main/kotlin/online/senpai/webo/dto/evolve/EvolveDialoguesMetaDto.kt
@@ -1,5 +1,8 @@
 package online.senpai.webo.dto.evolve
 
+import io.micronaut.core.annotation.Introspected
+
+@Introspected
 data class EvolveDialoguesMetaDto(
     val rowsNumber: Long,
     val rowsPerPage: Long
