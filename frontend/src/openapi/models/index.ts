30f1a8de9f4f7c1721ae70c53d654781bd0ebd70
Maxim Pavlov <maxpav137@gmail.com>
2022-03-16 12:33:24 +0500

    WEBO-19 chore(frontend): update OpenAPI generation

--- frontend/src/openapi/models/index.ts
+++ frontend/src/openapi/models/index.ts
@@ -1,5 +1,7 @@
 /* tslint:disable */
 /* eslint-disable */
+export * from './BlogPostDto'
+export * from './BlogPostPreviewDto'
 export * from './EvolveCharacter'
 export * from './EvolveDialogueDto'
 export * from './EvolveDialoguesMetaDto'
