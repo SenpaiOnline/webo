a944b0fd046d3638d41bf2693ea6cc532e3c3583
Maxim Pavlov <maxpav137@gmail.com>
2022-03-28 11:07:52 +0500

    WEBO-19 refactor(backend): add OpenAPI Schema annotation to BlogPostDto

--- backend/src/main/kotlin/online/senpai/webo/dto/blog/BlogPostDto.kt
+++ backend/src/main/kotlin/online/senpai/webo/dto/blog/BlogPostDto.kt
@@ -1,12 +1,13 @@
 package online.senpai.webo.dto.blog
 
 import io.micronaut.core.annotation.Introspected
+import io.swagger.v3.oas.annotations.media.Schema
 
 @Introspected
 data class BlogPostDto(
     val id: Long,
     val title: String,
-    val createdAt: String,
-    val updatedAt: String,
+    @Schema(format = "date-time") val createdAt: String,
+    @Schema(format = "date-time") val updatedAt: String,
     val content: String
 )
